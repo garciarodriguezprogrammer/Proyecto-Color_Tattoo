94e0443ec919e6af0fcbe4a6ce83f4816d2bb674
Dayanis <dayany1588@yahoo.com>
2024-02-28 18:26:59 +0100

    Se ha añadido la ruta para autenticación de administradores

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,8 @@ import express from "express";
 import appoinmentRoutes from "./routes/appointments.routes";
 import artistRoutes from "./routes/artists.routes";
 import authRoutes from "./routes/auth.routes";
-import userRoutes from "./routes/users.routes"
+import userRoutes from "./routes/users.routes";
+import adminAuthRoutes from "./routes/adminAuth.routes";
 // -----------------------------------------------------------------------------
 
 const router = express.Router();
@@ -11,5 +12,6 @@ router.use("/api/appointments", appoinmentRoutes);
 router.use("/api/auth", authRoutes);
 router.use("/api/users", userRoutes);
 router.use("/api/artist", artistRoutes);
+router.use("/api/adminAuth", adminAuthRoutes);
 
 export default router;
---
+++ src/routes/adminAuth.routes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { AdminAuthController } from "../controller/AdminAuthController";
+
+const router = express.Router();
+const authController = new AdminAuthController();
+//Registrar administrador
+router.post("/adminRegister", authController.adminRegister);  
+//Loguear administrador
+router.post("/adminLogin", authController.adminLogin);
+
+export default router;
