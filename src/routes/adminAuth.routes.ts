89d73f39796feaec6f8c70186eb55bd643be7c6e
Dayanis <dayany1588@yahoo.com>
2024-02-28 19:34:36 +0100

    Se ha añadido el README

--- src/routes/adminAuth.routes.ts
+++ src/routes/adminAuth.routes.ts
@@ -3,7 +3,7 @@ import { AdminAuthController } from "../controller/AdminAuthController";
 
 const router = express.Router();
 const authController = new AdminAuthController();
-//Registrar  administrador
+//Registrar administrador
 router.post("/adminRegister", authController.adminRegister);  
 //Loguear administrador
 router.post("/adminLogin", authController.adminLogin);
